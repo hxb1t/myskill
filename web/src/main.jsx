fb16c04c6931001ddf130ac220591b9262fb1c47
hxb1t <thepixel1975@gmail.com>
2026-05-24 12:01:23 +0700

    remove logrocket

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,13 +1,7 @@
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import ResetScroll from "./components/ResetScroll";
-import { BrowserRouter } from "react-router-dom";
-import { createRoot } from "react-dom/client";
-
-const appId = import.meta.env.VITE_LOGROCKET_APP_ID;
-
-if (appId && import.meta.env.PROD) {
-  LogRocket.init(appId);
-}
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter
@@ -18,5 +12,5 @@ createRoot(document.getElementById("root")).render(
   >
     <ResetScroll />
     <App />
-  </BrowserRouter>
+  </BrowserRouter>,
 );
